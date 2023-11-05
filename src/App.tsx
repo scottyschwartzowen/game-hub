443aa90035d8dee07c0f6fc3cda3e3146e1d8ebd
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-11-05 19:03:12 -0200

    Clean up the game cards

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <Box paddingLeft={10}>
+        <Box paddingLeft={3}>
           <GameHeading gameQuery={gameQuery} />
           <Flex marginBottom={5}>
             <Box marginRight={5}>
