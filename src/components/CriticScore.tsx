71475c15ccfc738b1af0d5baf42f8eb9a8b16dad
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-11-06 08:14:29 -0200

    Add emojis

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -5,7 +5,8 @@ interface Props {
 }
 
 const CriticScore = ({ score }: Props) => {
-  let color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';
+  let color =
+    score > 75 ? 'green' : score > 60 ? 'yellow' : score > 50 ? 'red' : '';
 
   return (
     <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="4px">
